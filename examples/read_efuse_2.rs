5173ef65f49bc6da2a6e953d774bd7f8b604c35e
toshiki goto <gotoshi.euphonium@gmail.com>
2023-07-17 16:32:49 +0900

    add sample

---
+++ examples/read_efuse_2.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use esp32c3::Peripherals;
+
+fn main() -> Result<()> {
+    esp_idf_sys::link_patches();
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = unsafe { Peripherals::steal() };
+    let data3_7 = peripherals.EFUSE.rd_usr_data7.read().bits();
+
+    println!("data3_7: {:x}", data3_7);
+
+    loop {
+        continue;
+    }
+}
